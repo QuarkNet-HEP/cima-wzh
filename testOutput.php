e441c1e911c2c250961fe2cf9d7f6439a83a6ec6
Doctor Krieger <16691642+JG-QuarkNet@users.noreply.github.com>
2019-06-06 14:01:07 -0400

    Removed .gitignore and test file from tracking

--- testOutput.php
+++
@@ -1,6 +0,0 @@
-
-Fatal error: Uncaught Error: Call to a member function fetch_object() on boolean in /home/quarkcat/sw/www-php/cima/test.php:32
-Stack trace:
-#0 /home/quarkcat/sw/www-php/cima/test.php(45): GetGroups(Array)
-#1 {main}
-  thrown in /home/quarkcat/sw/www-php/cima/test.php on line 32
