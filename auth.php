eb818e49b570987f239a811dfcb24e0080602056
Doctor Krieger <16691642+JG-QuarkNet@users.noreply.github.com>
2019-06-20 14:57:59 -0400

    Included correct config file and added comments

--- auth.php
+++ auth.php
@@ -2,16 +2,25 @@
 session_destroy();
 session_start();
 
-include "config/mc.config";
+// Include the config file with administrator login info
+//include "config/mc.config";
+include "../../local-settings/mc.config";
 
+// Get the login info from the config file
 $login = getAuthConfig();
+
+// User-entered login will be self-posted and processed here.
+// If the entered data matches $login, identify an authorized user to
+//   the Session and redirect to Classes.php
 if( isset($_POST["submit"]) && $_POST["username"]===$login["admin_login"] && $_POST["password"]===$login["admin_pw"] ){
 	$_SESSION["AUTHUSER"]=true;
 	header('Location: Classes.php');	
 }
 	
 include "templates/header.tpl";
-//print_r($_POST);
+
+// The HTML template for the login box:
 include "templates/auth.tpl";
+
 include "templates/floor.tpl";
 ?>
