de05eaabaa5aa908b500917bc453997be3f9a41e
JG-Quarknet <16691642+JG-QuarkNet@users.noreply.github.com>
2019-12-20 15:12:44 -0500

    Removed unused code and improved formatting

--- showGroups.php
+++ showGroups.php
@@ -3,24 +3,29 @@ include "database.php";
 
 
 $tables=explode(",",$_POST["tables"]);
-//$MCEvts[]="test";
-$k=0;
 //$groups=GetGroups($tables);
 $groups = getDatasetsByTable($tables);
 
 if($_POST["source"]=="index"){
-		echo '<div class=row align="center"><strong>Choose your group</strong></div>';
+		echo '<div class=row align="center">
+				 		<strong>Choose your group</strong>
+					</div>';
 }
 
 for($i=0;$i<count($groups);$i++){
 		if($_POST["source"]=="Backend"){
-				//echo '<option>'.$groups[$i]["dg_id"].'</option>';
+		
 				echo '<option>'.$groups[$i]["ds_id"].'</option>';
+
 		} elseif($_POST["source"]=="index") {
-/*			echo '<div class=row> <div class=col-md-3></div><div class=col-md-6 style="cursor: pointer;" align="center" id="'.$groups[$i]["dg_id"].'" onmouseover="OverCol(this)" onmouseout="OffCol(this)" onclick="GSel(this)">'.$groups[$i]["dg_id"].'</div></div>';*/
-			echo '<div class=row> <div class=col-md-3></div><div class=col-md-6 style="cursor: pointer;" align="center" id="'.$groups[$i]["ds_id"].'" onmouseover="OverCol(this)" onmouseout="OffCol(this)" onclick="GSel(this)">'.$groups[$i]["ds_id"].'</div></div>';
+
+				echo '<div class=row>
+					 			<div class=col-md-3></div>
+								<div class=col-md-6 style="cursor: pointer;" align="center"
+									 	 id="'.$groups[$i]["ds_id"].'"
+									 	 onmouseover="OverCol(this)" onmouseout="OffCol(this)"
+									 	 onclick="GSel(this)">'.$groups[$i]["ds_id"].'</div>
+							</div>';
 		}
 
 }
-
-
